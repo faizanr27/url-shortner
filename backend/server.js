19003892d1b39b6b68434083579a20251d5c0133
Faizan Raza <143700173+faizanr27@users.noreply.github.com>
2024-09-25 23:00:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://url-shortner-theta-eight.vercel.app/', 
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
