7e3b33676c889dbacd047311ed20d66aab216f32
Faizan Raza <143700173+faizanr27@users.noreply.github.com>
2024-09-29 10:41:23 -0700

    Update ProtectedRoute.jsx

--- frontend/src/pages/ProtectedRoute.jsx
+++ frontend/src/pages/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children, token }) {
-  return token ? children : <Navigate to="signin" replace/>;
+  const authToken = token || localStorage.getItem('token');
+  return authToken ? children : <Navigate to="/signin" replace />;
 }
 
 export default ProtectedRoute;
