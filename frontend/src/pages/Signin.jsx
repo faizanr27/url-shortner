35488cc78ba17620f25bb6135e2688fc172042d9
Faizan Raza <143700173+faizanr27@users.noreply.github.com>
2024-11-05 01:47:15 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -24,7 +24,7 @@ const Signin = () => {
     
         console.log('Data to be sent:', formData);
         try {
-            const response = await fetch('https://www.shortsy.xyz/api/authRoutes/login', {
+            const response = await fetch('https://shortsy.xyz/api/authRoutes/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
