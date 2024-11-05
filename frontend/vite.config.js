899c08807c26ba8fd5dba60a56fa6f08f24b1041
Faizan Raza <143700173+faizanr27@users.noreply.github.com>
2024-11-05 11:49:14 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-    historyApiFallback: true,
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  //   historyApiFallback: true,
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
